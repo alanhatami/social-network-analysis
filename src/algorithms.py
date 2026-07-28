3f7158a2381e8a603b70009f1f159155a58c5a99
mahdi <mmahdigh369@gmail.com>
2026-07-28 19:26:34 +0330

    feat(algo): add BFS shortest path algorithm

--- src/algorithms.py
+++ src/algorithms.py
@@ -39,3 +39,25 @@ def bfs_shortest_path(graph_obj, start_node, target_node):
          
                 queue.append(neighbor)
          return visited
+
+
+
+         def bfs_shortest_path(graph_obj, start_node, target_node):
+    if start_node == target_node:
+          return [start_node]
+      queue = deque([[start_node]])
+             visited = {start_node}
+    while queue:
+        path = queue.popleft()
+        node = path[-1]
+      
+      
+        for neighbor in graph_obj.graph.get(node, []):
+            if neighbor not in visited:
+                  new_path = list(path)
+                     new_path.append(neighbor)
+                queue.append(new_path)
+                   visited.add(neighbor)
+                  if neighbor == target_node:
+                       return new_path
+                          return None
