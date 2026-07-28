04328f5d0f2fe0fd03aa757db273aaa856e9604a
mahdi <mmahdigh369@gmail.com>
2026-07-28 20:56:03 +0330

    refactor(algo): optimize pathfinding memory efficiency

--- src/algorithms.py
+++ src/algorithms.py
@@ -39,25 +39,6 @@ def bfs_traversal(graph_obj, start_node):
     return visited
     
     
-def bfs_shortest_path(graph_obj, start_node, target_node):
-    if start_node == target_node:
-        return [start_node]
-    queue = deque([[start_node]])
-    visited = {start_node}
-    while queue:
-        path = queue.popleft()
-        node = path[-1]
-        for neighbor in graph_obj.graph.get(node, []):
-            if neighbor not in visited:
-                new_path = list(path)
-                new_path.append(neighbor)
-                queue.append(new_path)
-                visited.add(neighbor)
-                if neighbor == target_node:
-                    return new_path
-    return None
-
-
 def dfs_traversal(graph_obj, start_node):
     visited = []
     visited_set = {start_node}
