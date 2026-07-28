de90f2670d0261134d2403813b64438a3d560e7e
mahdi <mmahdigh369@gmail.com>
2026-07-28 16:55:32 +0330

    feat(src): implement BFS algorithm for shortest path

---
+++ src/algorithms.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+def bfs_shortest_path(graph_obj, start_node, target_node):
+    if start_node == target_node:
+        return [start_node]
+
+    queue = deque([[start_node]])
+    visited = {start_node}
+
+    while queue:
+        path = queue.popleft()
+        node = path[-1]
+
+        for neighbor in graph_obj.graph[node]:
+            if neighbor not in visited:
+                new_path = list(path)
+                new_path.append(neighbor)
+                queue.append(new_path)
+                visited.add(neighbor)
+
+                if neighbor == target_node:
+                    return new_path
+    return None
